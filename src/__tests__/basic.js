4ee113c43a1307e75b766968867bc32e0943692f
Dima Shirkov <dimashirkov@list.ru>
2022-10-17 17:28:10 +0300

    dak1 draft v1

---
+++ src/__tests__/basic.js
@@ -0,0 +1,46 @@
+import { calculateTotal } from '../calculate.js'; // позволяет извлеч пакет
+
+test('basic test', () => {
+    const result = 4;
+    expect(result).toBe(4);
+})
+
+test('calculateTotal sum', () => {
+    const list = [
+        {
+            id: 456,
+            name: 'Война и мир',
+            count: 3,
+            price: 400
+        },
+        {
+            id: 77,
+            name: 'JS',
+            count: 1,
+            price: 1300
+        }
+    ];
+
+    const result = calculateTotal(list);
+    expect(result).toBe(2500); // какой результат должен быть получен
+})
+
+test('calculateTotal sum with discount', () => {
+    const list = [
+        {
+            id: 456,
+            name: 'Война и мир',
+            count: 3,
+            price: 401
+        },
+        {
+            id: 77,
+            name: 'JS',
+            count: 1,
+            price: 1300
+        }
+    ];
+
+    const result = calculateTotal(list, true);
+    expect(result).toBeCloseTo(2230.173); // результат должен быть примерно равен данному значению
+})
---
+++ src/__tests__/user.js
@@ -0,0 +1,13 @@
+import loadUser from '../loadUser';
+import { httpGet } from '../http'; // подключение заглушки httpGet
+jest.mock('../http');
+
+beforeEach(() => { // сбрасывание изначальных данных 
+    jest.resetAllMocks();
+});
+
+test('should call loadUser once', () => {
+    httpGet.mockReturnValue(JSON.stringify({})); // httpGet должен вернуть строку
+    loadUser(1);
+    expect(httpGet).toBeCalledWith('http://server:8080/users/1'); // httpGet должна быть вызвана с параметром сервера
+}); 
