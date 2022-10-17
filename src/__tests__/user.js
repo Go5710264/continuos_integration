1a1cba082a05baa0592180e6ebef4e2b1843b778
Dima Shirkov <dimashirkov@list.ru>
2022-10-17 19:27:26 +0300

    fix eslint errors

--- src/__tests__/user.js
+++ src/__tests__/user.js
@@ -1,13 +1,14 @@
 import loadUser from '../loadUser';
-import { httpGet } from '../http'; // подключение заглушки httpGet
+import httpGet from '../http';
+// подключение заглушки httpGet
 jest.mock('../http');
 
-beforeEach(() => { // сбрасывание изначальных данных 
-    jest.resetAllMocks();
+beforeEach(() => { // сбрасывание изначальных данных
+  jest.resetAllMocks();
 });
 
 test('should call loadUser once', () => {
-    httpGet.mockReturnValue(JSON.stringify({})); // httpGet должен вернуть строку
-    loadUser(1);
-    expect(httpGet).toBeCalledWith('http://server:8080/users/1'); // httpGet должна быть вызвана с параметром сервера
-}); 
+  httpGet.mockReturnValue(JSON.stringify({})); // httpGet должен вернуть строку
+  loadUser(1);
+  expect(httpGet).toBeCalledWith('http://server:8080/users/1'); // httpGet должна быть вызвана с параметром сервера
+});
